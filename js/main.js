3175dd2eee1328130115448f3360d31f9496651f
Anton Romanov <609990@gmail.com>
2021-10-04 01:22:03 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -1,39 +1,35 @@
-let getRandomIntNumber = (min, max) => {
+const getRandomIntNumber = (min, max) => {
 
-    if (min >= max || min < 0 || max < 0) {
+  if (min >= max || min < 0 || max < 0) {
 
-        console.log('Значения до и после должны быть положительными числами, а значение до должно быть меньше значения после');
+    print('Значения до и после должны быть положительными числами, а значение до должно быть меньше значения после');
 
-    }
+  }
 
-    min = Math.ceil(min);
-    max = Math.floor(max);
+  min = Math.ceil(min);
+  max = Math.floor(max);
 
-    let Intnumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    return Intnumber;
-
+  const Intnumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  
+  return Intnumber;
 }
 
-console.log(getRandomIntNumber(0, 15));
+getRandomIntNumber(0, 15);
 
+const getRandomNumber = (min, max, count) => {
 
-let getRandomNumber = (min, max, count) => {
+  if (min >= max || min < 0 || max < 0) {
 
-    if (min >= max || min < 0 || max < 0) {
+    print('Значения до и после должны быть положительными числами, а значение до должно быть меньше значения после');
 
-        console.log('Значения до и после должны быть положительными числами, а значение до должно быть меньше значения после');
+  }
 
-    }
+  min = Math.ceil(min);
+  max = Math.floor(max);
 
-    min = Math.ceil(min);
-    max = Math.floor(max);
+  const number = Math.random() * (max - min + 1) + min;
 
-    let number = Math.random() * (max - min + 1) + min;
-
-    return number.toFixed(count);
-
+  return number.toFixed(count);
 }
 
-console.log(getRandomNumber(0, 15, 2));
-
-
+getRandomNumber(0, 15, 2);
